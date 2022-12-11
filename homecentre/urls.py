a0b1f310320485c7110ac9aa1370d2d5e9a155dd
Saurabh <saurabhadhau1@gmail.com>
2022-12-11 19:38:27 +0100

    created cart page and checkout page

--- homecentre/urls.py
+++ homecentre/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from cart.views import add_to_cart
+from cart.views import add_to_cart, cart, checkout
 from core.views import frontpage, shop, signup
 from product.views import product
 
@@ -16,6 +16,8 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('cart/', cart, name='cart'),
+    path('cart/checkout', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart' ),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
